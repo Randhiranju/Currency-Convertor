13ee76d5368bd3f5d3590800e4daca524b6fe744
Randhir Kumar Choudhary <randhiranju9355@gmail.com>
2023-10-06 12:13:43 +0530

    Initial Commit

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,57 @@
+import React, {useId} from 'react'
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd", //by default usd
+    amountDisable = false,
+    currencyDisable = false,
+    className = "", //taking css from user
+}) {
+   const amountInputId = useId()
+
+    return (
+        <>
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+            <div className="w-1/2">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
+                    {label}
+                </label>
+                <input
+                    id={amountInputId}
+                    className="outline-none w-full bg-transparent py-1.5"
+                    type="number"
+                    placeholder="Amount"
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                />
+            </div>
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <select
+                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)} // && is simply a checker to check if there is any available value in onCurrency change  else don't take
+                    disabled={currencyDisable}
+                >
+                    {/*map currencyOptions to get access of each currency
+                        key is passed in react for performance (if we don't pass react will just throw a warning but performance will be effected much)
+                    */}
+                        {currencyOptions.map((currency) => (
+                            <option key={currency} value={currency}>
+                            {currency}
+                            </option>
+                        ))}
+                
+                </select>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default InputBox;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+//this is just a better approach to wrap all components in this to export all component once
+
+import InputBox from "./InputBox";
+
+export {InputBox}
+// if we have more components we can directly export from here by using ,
+//export {comp1,comp2,.....} so that we don't require more import in app.jsx
